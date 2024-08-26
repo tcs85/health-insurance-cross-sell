371ac0771f5be7a4c0315f0a90c0cbb97558691b
thiago_souza <souza.eq@gmail.com>
2024-08-26 19:22:32 -0400

    fixing pickle path11

--- health_insurance/healthinsurance.py
+++ health_insurance/healthinsurance.py
@@ -14,16 +14,15 @@ class HealthInsurance:
         
     def data_cleaning(self, df1):
         ## 1.0 Rename Columns
-        cols_new = ['id', 'gender', 'age', 'region_code', 'policy_sales_channel', 'driving_license', 'vehicle_age', 'vehicle_damage', 'annual_premium', 'policy_sales_channel', 'vintage', 'response']
+        cols_new = ['id', 'gender', 'age', 'driving_license', 'region_code', 'previously_insured', 'vehicle_age', 
+                    'vehicle_damage', 'annual_premium', 'policy_sales_channel', 'vintage', 'response']
         df1.columns = cols_new
         return df1
         
     def feature_engineering(self, df2):
         ## 2.0 Feature Engineering
         # vehicle age
-        df2['vehicle_age'] = df2['vehicle_age'].apply( lambda x: 'over 2 years' if x == '> 2 Years' 
-                                                          else 'between 1-2 years' if x == '1-2 Year' 
-                                                          else 'under 1 year')
+        df2['vehicle_age'] = df2['vehicle_age'].apply( lambda x: 'over_2_years' if x == '> 2 Years' else 'between_1_2_year' if x == '1-2 Year' else 'below_1_year' )
         # vehicle damage
         df2['vehicle_damage'] = df2['vehicle_damage'].apply( lambda x: 1 if x == 'Yes' else 0)
         
@@ -41,31 +40,29 @@ class HealthInsurance:
         df5['vintage'] = self.vintage_scalar.transform(df5[['vintage']].values)
         
         # gender - Target Encoding
-        df5.loc[:, 'gender'] = df5.loc[:, 'gender'].map(self.target_encode_gender)
+        df5.loc[:, 'gender'] = df5.loc[:, 'gender'].map(self.target_encode_gender).astype('float64') 
         
         # region_code - Target Encoding
-        df5.loc[:, 'region_code'] = df5.loc[:, 'region_code'].map(self.target_encode_region_code)
+        df5.loc[:, 'region_code'] = df5.loc[:, 'region_code'].map(self.target_encode_region_code).astype('float64')
         
         # policy_sales_channel - Frequency Encoding
-        df5.loc[:, 'policy_sales_channel'] = df5.loc[:, 'policy_sales_channel'].map(self.fe_policy_sales_channel)
+        df5.loc[:, 'policy_sales_channel'] = df5.loc[:, 'policy_sales_channel'].map(self.fe_policy_sales_channel).astype('float64')
         
         # Vehicle Age
         df5 = pd.get_dummies(df5, prefix=['vehicle_age'], columns=['vehicle_age'])
         
-        # Reset Index
-        df5 = df5.reset_index(drop=True)
-                
         # Featuring Selection
-        cols_selected = ['vintage', 'annual_premium', 'age', 'region_code', 'policy_sales_channel', 'previously_insured', 'vehicle_damage', 'vehicle_age_over 2 years', 'vehicle_age_under 1 year']
+        cols_selected = ['annual_premium', 'vintage', 'age', 'region_code', 'vehicle_damage', 'previously_insured',
+                         'policy_sales_channel']
     
         return df5 [cols_selected]
     
-    def get_prediction(self, model, original_data, test_data):
-        # prediction
-        pred = model.predict_proba(test_data)
+    def get_prediction( self, model, original_data, test_data ):
+        # model prediction
+        pred = model.predict_proba( test_data )
+
+        # join prediction into original data
+        original_data['score'] = pred[:, 1].tolist()
         
-        # join pred into the original data
-        original_data['score'] = pred
-        
-        return original_data.to_json(orient='records', date_format='iso')
+        return original_data.to_json( orient='records', date_format='iso' )
     
